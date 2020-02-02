d172c5a811c896edb161be252d125c32be0aa798
mphobwcodes <36450959+mphobwcodes@users.noreply.github.com>
2020-02-02 13:50:58 -0500

    Day1

---
+++ PrintArtists.java
@@ -0,0 +1,25 @@
+package Homeworks;
+
+public class PrintArtists {
+
+	public static void main(String[] args) {
+		//variables defined and initialized
+		
+		int yearL = 1432;
+		int yearM = 1475;
+		int yearV = 1853;
+		int yearA = 1840;
+		int yearP = 1881;
+		int yearS = 1901;
+		
+		System.out.printf(" \n%d   Leanardo da Vinci", yearL);
+		System.out.printf("\n%d   Michal Angelo", yearM);
+		System.out.printf(" \n%d   Vincent Van Gogh", yearV);
+		System.out.printf(" \n%d   Auguste Rodin", yearA);
+		System.out.printf(" \n%d   Pablo Picasso", yearP);
+		System.out.printf(" \n%d   Salvador Dali", yearS);
+		
+
+	}
+
+}
---
+++ Shapes.java
@@ -0,0 +1,17 @@
+package Homeworks;
+
+public class Shapes {
+
+	public static void main(String[] args) {
+		System.out.println("This application displays a box and Oval using asteriks(*)");
+		System.out.println("*******        *");
+		System.out.println("*     *       ***");
+		System.out.println("*     *      *****");
+		System.out.println("*     *        *");
+		System.out.println("*     *        *");
+		System.out.println("*     *        *");
+		System.out.println("*******        *");
+	
+	}
+
+}
